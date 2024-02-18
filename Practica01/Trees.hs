5491ca64834cf4133cb96a84078dbbb7ce9ce314
Eduardo Martínez <111658253+emtz646@users.noreply.github.com>
2024-02-17 18:27:11 -0600

    All functions implemented

--- Practica01/Trees.hs
+++ Practica01/Trees.hs
@@ -7,8 +7,10 @@
 - Laboratorio: José Ricardo Desales Santos
 - Practica 1: Recordando Haskell. Árboles
 - Integrantes:
--
--
+- Arias Villaroel Alejandra Valentina
+- Del Monte Ortega Maryam Michelle
+- Martínez Mejía Eduardo
+- Monroi Romero Sahara Mariel
 -}
 
 module Trees where
@@ -23,39 +25,56 @@ data BTree a = Void | Node a (BTree a) (BTree a) deriving (Show, Eq)
 
 -- | Regresa el número de nodos de un árbol.
 nNodes :: BTree a -> Int
-nNodes = error "D:"
+nNodes Void = 0 --Si el arbol es vacio, tiene 0 nodos
+nNodes (Node n h1 h2) = 1 + nNodes h1 + nNodes h2 -- Cuenta el primer nodo y luego cada hijo de este de forma recursiva
 
 -- | Regresa el número de hojas de un árbol.
 nLeaves :: BTree a -> Int
-nLeaves = error "D:"
+nLeaves Void = 0 --Si el arbol es vacio, no tiene hojas
+nLeaves (Node n Void Void) = 1 --Si el nodo no tiene hijos, entonces es una hoja
+nLeaves (Node n h1 h2) = nLeaves h1 + nLeaves h2 --Recorre los hijos de cada nodo recursivamente hasta encontrar una hoja y sumar 1
 
 -- | Regresa el número de nodos internos de un árbol.
 nni :: BTree a -> Int
-nni = error "D:"
+nni Void = 0 --Si el arbol es vacio, no tiene nodos
+nni (Node n Void Void) = 0 --Si el nodo es hoja, entonces no se cuenta
+nni (Node n h1 h2) = 1 + nni h1 + nni h2 --Cuenta el primer nodo y luego cada hijo de este de forma recursiva excepto las hojas
 
 -- | Nos dice si un elemento está contenido en un árbol ordenado.
 contains :: (Ord a, Eq a) => a -> BTree a -> Bool
-contains = error "D:"
+contains a Void = False --Si el arbol es vacio, el elemento no esta
+contains a (Node n h1 h2) = a == n || contains a h1 || contains a h2 --Busca si el elemento esta en el nodo. SI no se encuentra se busca recursivamente en sus hijos.
 
 -- | Recorrido inorder.
 inorder :: BTree a -> [a]
-inorder = error "D:"
+inorder Void = [] --Si el arbol es vacio, regresa una lista vacia
+inorder (Node n h1 h2) = inorder h1 ++ [n] ++ inorder h2 --Agrega primero los hijos izquierdos, luego el nodo padre y al ultimo los hijos derechos a una lista
 
 -- | Recorrido preorder.
 preorder :: BTree a -> [a]
-preorder = error "D:"
+preorder Void = [] --Si el arbol es vacio, regresa una lista vacia
+preorder (Node n h1 h2) = [n] ++ preorder h1 ++ preorder h2 --Agrega primero el nodo padre, luego los hijos izquierdos y al ultimo los derechos en una lista
 
 -- | Recorrido postorder.
 postorder :: BTree a -> [a]
-postorder = error "D:"
+postorder Void = [] --Si el arbol es vacio, regresa una lista vacia
+postorder (Node n h1 h2) = postorder h1 ++ postorder h2 ++ [n] -- Agrega primero los hijos izquierdos, luego los derechos y al ultimo al nodo padre en una lista
 
 -- | Agrega un elemento a un árbol binario de manera ordenada.
 add :: (Ord a) => a -> BTree a -> BTree a
-add = error "D:"
+add a Void = Node a Void Void --Si el arbol es vacio, introduce el elemento como cabeza
+add a (Node n h1 h2) 
+  | a < n =  Node n (add a h1) h2 --Si el elemento nuevo es menor al nodo,  lo agrega al subarbol izquierdo
+  | a > n = Node n h1 (add a h2) --Si el elemento nuevo es mayor al nodo, lo agrega al subarbol derecho
+  | otherwise = Node n h1 h2 -- En caso de que el elemento nuevo sea igual al nodo, significa que el elemento ya existe y no se agrega, regresando la misma lista
 
--- | Pasa una lista a un árbol binario de forma ordenada.
+
+-- | Pasa una lista a un árb ol binario de forma ordenada.
 fromList :: (Ord a) => [a] -> BTree a -> BTree a
-fromList = error "D:"
+fromList [] t = t --Si la lista a agregar es vacia, se regresa el mismo arbol
+fromList (x:xs) Void = fromList xs (add x Void) --Si el arbol es vacio, se crea un nodo inicial con el primer elemento de la lista y recursivamente se agrega el resto de la lista
+fromList (x:xs) t = fromList xs (add x t) -- Se agrega el primer elemento de la lista y recursivamente se agrega el resto de la lista
+
 
 --------------------------------------------------------------------------------
 --------                           AUXILIARES                           --------
